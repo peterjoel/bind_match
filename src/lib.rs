29c0dd4ff7d1d89bedf0b7aa3f250d0d547182ad
Peter Hall <peterjoel@gmail.com>
2021-01-06 19:47:25 +0000

    fixed docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-/// Works similarly to `matches!` but returns an `Option` of variables bound in the pattern
+/// Works similarly to `matches!` but returns an `Option` of variables bound in the pattern.
 ///
 /// The input of the macro looks like this:
 ///
@@ -26,12 +26,12 @@
 /// }
 ///
 /// # fn main() {
-///    let bar = Bar {
-///        foo: Foo::A(Some(42), true),
-///        fun: false,
-///    };
-///    let result = bind_match!(bar, Bar { foo: Foo::A(Some(n), x), fun } if !fun => (n, x));
-///    assert_eq!(result, Some((42, true)));
+/// let bar = Bar {
+///     foo: Foo::A(Some(42), true),
+///     fun: false,
+/// };
+/// let result = bind_match!(bar, Bar { foo: Foo::A(Some(n), x), fun } if !fun => (n, x));
+/// assert_eq!(result, Some((42, true)));
 /// # }
 /// ```
 /// This can be particularly useful when matching inside iterator adapters. For example, using
@@ -47,12 +47,11 @@
 /// #     fun: bool,
 /// # }
 /// # fn main() {
-/// let bar = Bar {
-///     foo: Foo::A(Some(42), true),
-///     fun: false,
-/// };
-/// let result = bind_match!(bar, Bar { foo: Foo::A(Some(n), x), fun } if !fun => (n, x));
-/// assert_eq!(result, Some((42, true)));
+/// fn fun_when_open(bars: impl IntoIterator<Item = Bar>) -> Option<bool> {
+///     bars.into_iter()
+///         .filter_map(|bar| bind_match!(bar, Bar { foo: Foo::B { open, .. }, fun } if open => fun ))
+///         .next()
+/// }
 /// # }
 /// ```
 /// The binding expression can be omitted, in which case it defaults to `Some(())`:
